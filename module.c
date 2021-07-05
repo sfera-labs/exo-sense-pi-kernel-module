3b4e580c1280af15c1c4fbe7b18d338793fe8b2f
bancha321 <82613603+bancha321@users.noreply.github.com>
2021-07-05 09:37:31 +0200

    version update

--- module.c
+++ module.c
@@ -53,7 +53,7 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sfera Labs - http://sferalabs.cc");
 MODULE_DESCRIPTION("Exo Sense Pi driver module");
-MODULE_VERSION("1.1");
+MODULE_VERSION("1.2");
 
 static int temp_calib_m = -1000;
 module_param(temp_calib_m, int, S_IRUGO);
