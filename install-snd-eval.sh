444fa963559551c0b3872d52ad66192fb5ed49f2
bancha321 <82613603+bancha321@users.noreply.github.com>
2021-08-04 10:19:35 +0200

    updated installation script for when sound-eval.service was previously installed

--- install-snd-eval.sh
+++ install-snd-eval.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sudo apt-get -y install libasound2-dev
 sudo apt-get -y install libfftw3-dev
+sudo systemctl stop sound-eval.service
+sudo systemctl disable sound-eval.service
 sudo cp ./sound-eval/sound-eval.sh /usr/local/bin/
 sudo cp ./sound-eval/soundEval /usr/local/bin/
 sudo cp ./sound-eval/sound-eval.service /etc/systemd/system/ 
 sudo chmod 744 /usr/local/bin/sound-eval.sh
 sudo chown root.root /usr/local/bin/sound-eval.sh
 sudo systemctl enable sound-eval.service
-
+sudo systemctl start sound-eval.service
