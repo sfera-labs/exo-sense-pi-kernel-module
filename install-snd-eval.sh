3db0ff8628f56e2fcbe0ed25edc7eff105844c0f
Giampiero <giampiero.baggiani@gmail.com>
2022-02-22 16:10:39 +0100

    Added soundEval 64-bit version

--- install-snd-eval.sh
+++ install-snd-eval.sh
@@ -4,8 +4,9 @@ sudo apt-get -y install libfftw3-dev
 sudo systemctl stop sound-eval.service
 sudo systemctl disable sound-eval.service
 sudo cp ./sound-eval/sound-eval.sh /usr/local/bin/
-sudo cp ./sound-eval/soundEval /usr/local/bin/
-sudo cp ./sound-eval/sound-eval.service /etc/systemd/system/ 
+uname -m | grep -q 'aarch64' && SNDEVBIN="soundEval64" || SNDEVBIN="soundEval"
+sudo cp ./sound-eval/$SNDEVBIN /usr/local/bin/soundEval
+sudo cp ./sound-eval/sound-eval.service /etc/systemd/system/
 sudo chmod 744 /usr/local/bin/sound-eval.sh
 sudo chown root.root /usr/local/bin/sound-eval.sh
 sudo systemctl enable sound-eval.service
