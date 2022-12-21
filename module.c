54eef9cd11be456cb02878874d634a5fe0451a1e
Giampiero <giampiero.baggiani@gmail.com>
2022-12-21 14:08:17 +0100

    Updated gpio lib

--- module.c
+++ module.c
@@ -61,7 +61,7 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sfera Labs - http://sferalabs.cc");
 MODULE_DESCRIPTION("Exo Sense Pi driver module");
-MODULE_VERSION("2.9");
+MODULE_VERSION("2.10");
 
 static int temp_calib_m = -1000;
 module_param( temp_calib_m, int, S_IRUGO);
